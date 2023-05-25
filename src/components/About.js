b70eae3461757b2a7faf586b3476bbecb599ff5b
Joseph Ofei <josephofei24@gmail.com>
2023-05-24 22:21:19 -0400

    props basic compolete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,34 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+
+function About(props) {
+
+  // const isbio = function () {
+  //   if (props.bio === "" && props.bio === undefined) {
+  //     return;
+  //   } else {
+  //     return props.bio
+  //   }
+  // }
+
+
+
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+      {/* <p>{isbio()}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
+
+
+
+
     </div>
   );
 }
 
+{/* <Links URL="https://github.com/liza" /> */ }
 export default About;
